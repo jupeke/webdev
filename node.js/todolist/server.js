c8c3011097db5d72d60760a1c1eab9216ede97b2
Jukka-Pekka Kerkkanen <kerkkaju@teacher.eursc.eu>
2018-05-20 19:46:06 +0200

    Todo works partially..

--- node.js/todolist/server.js
+++ node.js/todolist/server.js
@@ -1,14 +1,33 @@
-var express = require('express');
+const express = require('express');
+const cookieSession = require('cookie-session');
 var app = express();
+
+var list = ["Faire les courses", "Nourrir le chat",
+          "Arroser les plantes"];
+
+app.use(cookieSession({
+  nam: 'session',
+  keys: ['key1','key2'] // Meaning unsure..
+}));
 
 // Set view engine to ejs.
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
-  let list = ["Faire les courses", "Nourrir le chat",
-            "arroser les plantes"];
+  if(isNaN(req.session.list)){
+    req.session.list = list;
+  }
+  let currentList = req.session.list;
   res.render('list',{
-    list: list;
+    list: currentList
+  });
+});
+
+app.get('/add', function(req, res) {
+  var devoir = req.query.todo;  // Note query!
+  req.session.list.push(devoir);
+  res.render('list',{
+    list: req.session.list
   });
 });
 
@@ -17,5 +36,16 @@ app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/html');
     res.status(404).send('Page introuvable !');
 });
+
+// Removes one element with given id from the given array (=list).
+// Removes also the corresponding html element.
+var remove = function(id){
+
+  var elem = document.getElementById(id);
+  var parent = elem.parentNode;
+  parent.removeChild(elem);
+
+  //return list.splice(id,1);
+}
 
 app.listen(8081);
