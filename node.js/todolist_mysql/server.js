5b8ce19f3a4ed24dfdbb06f3cb7a7a3f534881dc
Jukka-Pekka Kerkkanen <kerkkaju@teacher.eursc.eu>
2018-06-02 13:08:50 +0200

    Works! A big step for a human!

--- node.js/todolist_mysql/server.js
+++ node.js/todolist_mysql/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const mysql = require('mysql');
+var mysql = require('mysql');
 
 //const cookieSession = require('cookie-session');
 var app = express();
-
-var list = ["Faire les courses", "Nourrir le chat",
-          "Arroser les plantes"];
 
 // Opens database connection:
 var dbc = mysql.createConnection({
@@ -14,16 +11,29 @@ var dbc = mysql.createConnection({
   password: "",
   database: "notes"
 });
+dbc.connect();
+//console.log('connected as id ' + dbc.threadId); Gives null...
 
 // Set view engine to ejs.
 app.set("view engine", "ejs")
 
 .get('/', function(req, res) {
-  let currentList = getList(dbc);
-  res.render('list',{
-    list: currentList
-  });
+  getList(dbc, res, myCallback);
 })
+
+// This works as well. The callback function is anonymous
+// which may be more difficult to read, but concise.
+/*.get('/', function(req, res) {
+  getList(dbc, res, function(err, currentList){
+    if(err){
+      throw err;
+    }
+    console.log("All togehter version");
+    res.render('list',{
+      list: currentList
+    }); // end of res.render
+  }); // end of getList
+})*/  // end of get
 
 .get('/add', function(req, res) {
   var devoir = req.query.todo;  // Note query!
@@ -33,7 +43,15 @@ app.set("view engine", "ejs")
 
 .get('/remove', function(req, res) {
   var id = req.query.id;  // Note query!
-  removeTask(dbc,id);
+  console.log("id="+id);
+  removeTask(dbc,id, function(err, result){
+    if(err){
+      throw err;
+      console.log("Error while removing task");
+    } else{
+      console.log("Task removed ok!");
+    }
+  }); // end of removeTask
   res.redirect('/');
 })
 
@@ -51,43 +69,61 @@ app.set("view engine", "ejs")
 .listen(8081);
 
 // Makes a DB query and return an array containing all
-// the wanted tasks:
-var getList = function (connection){
-  var tasks = [];
+// the wanted tasks. Note the use of a callback function!
+var getList = function (connection, res, callback){
 
-  var query = "Select * from notes " +
-          " where done = 0 " +
-          " order by modified";
-
-  dbc.query(query, function (error, results, fields) {
+  var query = "Select id, task from todolist " +
+          "where done = 0 " +
+          "order by edited";
+  console.log(query);
+  connection.query(query, function (error, results, fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
-    tasks = results;
+    if (error){
+      throw error;
+      callback(error, false, res);
+    } else{
+      callback(null, results, res);
+    }
   });
+}
 
-  return tasks;
+// Here the callback for the getList function. Made all
+// the possible mistakes before figuring out the idea...
+var myCallback = function(err, currentList, res){
+  if(err){
+    throw err;
+  }
+  console.log("currentList len ="+currentList.length);
+  res.render('list',{
+    list: currentList
+  });
 }
 
 // Makes a DB delete query and returns an error variable that
 // is an Error if dbc.query encounteres one.
-var removeTask = function (connection, id){
-  var tasks = [];
+var removeTask = function (connection, id, callb){
+  var result = false;
 
   // This is a good way to escape query values:
-  var rawQuery = "Delete from notes " +
-                " where id = ?";
+  var rawQuery = "Delete from todolist " +
+                "where id = ?";
   var inserts = [id];
   var query = mysql.format(rawQuery, inserts);
 
-  dbc.query(query, function (error, results, fields) {
+  connection.query(query, function (error, results, fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
-    tasks = results;
+    if (error){
+      throw error;  // Meaning unclear.
+      callb(error, false);
+    } else{
+      callb(null, results);
+    }
   });
 
-  return error;
 }
 
 // Makes a DB query and returns an error variable that
@@ -95,20 +131,22 @@ var removeTask = function (connection, id){
 var addTask = function (connection, newTask){
   var d = new Date();
   var time = d.getTime(); // Created & modified
-
-  var rawQuery = "Insert into notes " +
-          " values(?, 0, ?, ?)";
+  time = 123;
+  var result = false;
+  var rawQuery = "Insert into todolist (task, done, created, edited) " +
+          "values(?,0,?,?)";
 
   var inserts = [newTask, time, time];
   var query = mysql.format(rawQuery, inserts);
+  console.log(query);
 
   dbc.query(query, function (error, results, fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
-  });
+    result = error;
 
-  return error;
+  });
 }
 
-dbc.end();
+//dbc.end();
