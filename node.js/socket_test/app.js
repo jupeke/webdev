5c44979db18e1c68ebd93a534cb9668fd49ea601
Jukka-Pekka Kerkkanen <kerkkaju@teacher.eursc.eu>
2018-06-30 13:14:55 +0200

    Testing messages with socket.io

---
+++ node.js/socket_test/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./views/index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+    socket.emit('messages', 'Salut, mon petit client!')
+});
+
+server.listen(8082);
