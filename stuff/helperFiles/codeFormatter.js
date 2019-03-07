41e36972ecd157eed459ff4e6871d9828c54262a
Jukka-Pekka <jpkerkkanen@gmail.com>
2019-03-07 21:47:43 +0100

    Edited codeFormatter. It's not too easy to format code.

--- stuff/helperFiles/codeFormatter.js
+++ stuff/helperFiles/codeFormatter.js
@@ -1,9 +1,10 @@
 class CodeFormatter {
-  constructor(code){
-    this.code = code;
+  constructor(){
+    this.code = "";
 
     this.keywordStyle = "color: blue";
     this.funcNameStyle ="weight: bold";
+    this.operatorStyle ="color: green";
 
     this.keywords = [
       "function",
@@ -13,7 +14,22 @@ class CodeFormatter {
       "this",
       "if",
       "else"
-    ]
+    ];
+    this.operators = [
+      "+",
+      "++",
+      "=",
+      "==",
+      "===",
+      "-",
+      "--",
+      "<",
+      ">",
+      "+=",
+      "-=",
+      ">=",
+      "<="
+    ];
   }
 
   getCode(){
@@ -25,22 +41,52 @@ class CodeFormatter {
     }
   }
 
-  // Colors keywords etc to make code nicer. Returns the result html.
+  // Does all the formatting to the code. Colors keywords etc to make code nicer.
+  // Returns the final html.
   format(codeBlock){
     let formattedCode = codeBlock.replace(/</g, "&lt;");
+    formattedCode = formattedCode.replace(/>/g, "&gt;");
 
+    formattedCode = this.formatKeywords(formattedCode);
+
+    // After this the code contains a lot of html markup which makes
+    // it more difficult to go on. Probably a different approuch needed.
+    //formattedCode = this.formatOperators(formattedCode);
+
+    return formattedCode;
+  }
+
+  // Does all the formatting to the code. Colors keywords etc to make code nicer.
+  // Returns the final html.
+  formatKeywords(codeBlock){
+    let formattedCode = codeBlock;
     for(let i=0; i < this.keywords.length; i++){
 
       let keyword = this.keywords[i];
-      var regExp = new RegExp(keyword,"g");  // This way variable works.
+      let regExp = new RegExp(keyword,"g");  // This way variable works.
 
       formattedCode = formattedCode.replace(regExp,
         "<span style='"+this.keywordStyle+"'>"+keyword+"</span>");
+    }
+
+    return formattedCode;
+  }
+
+  // Formats JS operator like +-==<
+  formatOperators(codeBlock){
+    let formattedCode = codeBlock;
+    for(let i=0; i < this.operators.length; i++){
+
+      let operator = this.operators[i];
+      let regExp = new RegExp(operator,"g");
+
+      formattedCode = formattedCode.replace(regExp,
+        "<span style='"+this.operatorStyle+"'>"+operator+"</span>");
     }
     return formattedCode;
   }
 
   getSubstring(){
-    test.match(new RegExp(firstvariable + "(.*)" + secondvariable));
+    test.match(new RegExp(var1 + "(.*)" + var2));
   }
 }
