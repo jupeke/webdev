c20033ef9f09dee27531bf62403563ee62700191
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2022-01-26 10:04:25 +0100

    Update links.php

--- php/links.php
+++ php/links.php
@@ -1,6 +1,6 @@
 <?php
   $html =
-  '<li><a href="https://www.w3schools.com">w3schools jlö</a></li>
+  '<li><a href="https://www.w3schools.com">w3schools</a></li>
   <li><a href="https://github.com/kerkkaju/webdev/tree/master/php" target="_blank">Codes
     </a>
   </li>';
