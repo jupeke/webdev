6c61eb5cd9754b929ff493ae19d22e997727ed42
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2022-02-02 10:05:19 +0100

    Update form_with_db.php

--- php/form_with_db.php
+++ php/form_with_db.php
@@ -3,6 +3,8 @@
     // Constants (false to make name case-sensitive). Syntax:
     // define(name, value, case-insensitive)
     define("SAVE_NEW", "Save new comment", false);
+    define("EDIT", "Edit", false);
+    define("DEL", "Delete", false);
 
     $error = "";
     $comment = "";
@@ -26,25 +28,31 @@
     // If a form with POST method has been sent:
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user_action =
-            isset($_REQUEST["user_action"]) ? $_REQUEST["user_action"]: "";
+            isset($_REQUEST["user_action"]) ? $_REQUEST["user_action"]: "none";
 
         // User actions:
-        if($user_action = ){
-          
-        }
-
-        if(isset($_POST["comment"])){
-            $comment = $_POST["comment"];
-            if(empty($comment)){
-                $message = 'Comment is empty! Write a comment, please!';
-            } else{
-                if(comment_insert($comment, $conn)){
-                    $message = 'Comment saved successfully';
+        if($user_action = "SAVE_NEW"){
+            if(isset($_POST["comment"])){
+                $comment = $_POST["comment"];
+                if(empty($comment)){
+                    $message = 'Comment is empty! Write a comment, please!';
                 } else{
-                    $message = 'Error in saving a comment: '.$conn->error;
+                    if(comment_insert($comment, $conn)){
+                        $message = 'Comment saved successfully';
+                    } else{
+                        $message = 'Error in saving a comment: '.$conn->error;
+                    }
+                    $details_ok = True;
                 }
-                $details_ok = True;
             }
+        } else if($user_action = "DEL"){
+            $id = isset($_GET["id_comment"]) ? $_GET["id_comment"]: -1;
+            if(comment_delete($id, $conn)){
+                $message = 'Comment deleted successfully';
+            } else{
+                $message = 'Error in deleting a comment: '.$conn->error;
+            }
+            $details_ok = True;
         }
     }
     $head =
@@ -83,10 +91,21 @@
     echo $html;
 
 
-    // Inserts a new comment into comments table. Return True or false
+    // Insert a new comment into comments table. Return True or false
     // based on success
     function comment_insert($comment, $connection){
         $sql = "INSERT INTO comments (comment) VALUES ('".$comment."')";
+        if ($connection->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Delete a comment with id=$id_comment from comments. Return True or false
+    // based on success
+    function comment_delete($id_comment, $connection){
+        $sql = "DELETE FROM comments WHERE id=".$id_comment;
         if ($connection->query($sql) === TRUE) {
             return true;
         } else {
@@ -101,9 +120,17 @@
         if ($result->num_rows > 0) {
             // Extract the comments:
             while($row = $result->fetch_assoc()) {
+                $id = $row["id"];
                 $output .= $row["comment"]."<br>";
             }
         }
         return $output;
+    }
+    // Create a new form element containing an input element.
+    function create_delete_button($id_comment){
+        $btn =
+          '<form method="post" action="form_with_db.php?id_comment='.$id_comment.'"?>
+            <input type="submit" name="user_action" value="'.DEL.'">
+          </form>';
     }
 ?>
