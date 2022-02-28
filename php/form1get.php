b082ae312bcd9c01fdb5024fa47f7a424af440ee
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2022-02-28 10:10:19 +0100

    add form1get.php

---
+++ php/form1get.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>PHP Home</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+
+  <body>
+    <ul id="linkbar">
+      <?php include 'links.php'; ?>
+      <?php echo $links_html; ?>
+    </ul>
+    <h1>My first form</h1>
+    <?php
+      if(isset($_POST["name"])){
+          echo "Hello ".$_POST["name"];
+      } else{
+          echo "No name given" ;
+      }
+    ?>
+    <p>Write you name in the text field and press the button.</p>
+    <form method="post">
+      <label for="name">Your name:</label><br>
+      <input type="text" id="name" name="name" value=""><br>
+      <input type="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
--- php/testing.php
+++ php/testing.php
@@ -14,7 +14,7 @@
     <?php
         $result = "";
         for($i = 0; $i < 10; $i++){
-          for($k = 0; $k < 70; $k++){
+          for($k = 0; $k < 20; $k++){
             $result .= "*";
           }
           $result .= "<br>";
