0244a6cb3b212ea774ea40a5edff36ff6158116d
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2022-04-01 13:35:59 +0200

    link bar - dropdown links added

--- php/links.php
+++ php/links.php
@@ -1,11 +1,30 @@
 <?php
   $links_html =
   '<li><a href="index.php">Home</a></li>
-  <li><a href="form1.php">Form 1 (POST)</a></li>
-  <li><a href="form1get.php">Form 1 (GET)</a></li>
-  <li><a href="form1_php_first.php">Form 1 (php_1st)</a></li>
-  <li><a href="form2.php">Form 2</a></li>
-  <li><a href="persistent.php">Persistent comments</a></li>
+  <li>
+    <div class="dropdown">
+      <a class="dropbtn">Forms</a>
+      <div class="dropdown-content">
+        <a href="form1.php">Form 1 (POST)</a>
+        <a href="form1get.php">Form 1 (GET)</a>
+        <a href="form1_php_first.php">Form 1 (php_1st)</a>
+        <a href="form2.php">Form 2</a>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="dropdown">
+      <a class="dropbtn">Persistent comments</a>
+      <div class="dropdown-content">
+        <a href="persistent_step0.php">Starting point</a>
+        <a href="persistent_step1.php">Read and Write</a>
+        <a href="persistent_step2.php">Delete</a>
+        <a href="persistent_step3.php">Update</a>
+        <a href="persistent.php">Final</a>
+      </div>
+    </div>
+  </li>
+  
   <li><a class="out" href="https://www.w3schools.com/php"
     target="_blank">Tutorial</a></li>
   <li><a class="out" href="https://github.com/kerkkaju/webdev/tree/master/php"
