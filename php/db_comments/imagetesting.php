c7063f4d55d77187d947fc56ff10cec8d984cac2
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2022-05-17 15:18:00 +0200

    Update imagetesting.php

--- php/db_comments/imagetesting.php
+++ php/db_comments/imagetesting.php
@@ -332,4 +332,45 @@ EOF
           </form>';
         return $form;
     }
+    // Found at https://www.php.net/manual/en/function.openssl-public-encrypt.php#95307
+    function ssl_encrypt($source,$type,$key){
+        //Assumes 1024 bit key and encrypts in chunks.
+
+        $maxlength=117;
+        $output='';
+        while($source){
+          $input= substr($source,0,$maxlength);
+          $source=substr($source,$maxlength);
+          if($type=='private'){
+            $ok= openssl_private_encrypt($input,$encrypted,$key);
+          }else{
+            $ok= openssl_public_encrypt($input,$encrypted,$key);
+          }
+
+          $output.=$encrypted;
+        }
+        return $output;
+    }
+
+    function ssl_decrypt($source,$type,$key){
+        // The raw PHP decryption functions appear to work
+        // on 128 Byte chunks. So this decrypts long text
+        // encrypted with ssl_encrypt().
+
+        $maxlength=128;
+        $output='';
+        while($source){
+          $input= substr($source,0,$maxlength);
+          $source=substr($source,$maxlength);
+          if($type=='private'){
+            $ok= openssl_private_decrypt($input,$out,$key);
+          }else{
+            $ok= openssl_public_decrypt($input,$out,$key);
+          }
+
+          $output.=$out;
+        }
+        return $output;
+
+    }
 ?>
