c8fe191f27620f6465f3e3ce42ac0b813e648ae3
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-09-26 09:52:00 +0200

    Update app.py

--- webpy/project3/app.py
+++ webpy/project3/app.py
@@ -1,22 +1,34 @@
+from os import uname
 import web
 render = web.template.render('templates/')
 urls = (
     '/', 'Login',
-    '/success/','Check',
-    '/failure/','Check',
+    '/success/','Success',
+    '/failure/','Failure',
 )
 class Login:
     def GET(self):
         return render.login()   # home is the template name
-
-class Check:
-    def GET(self):
-        content = "OK"
+    def POST(self):
+        user_input = web.input()
+        username = user_input.username
+        password = user_input.password
         success = True
         if(success):
             return render.success(content)    
         else:
             return render.failure(content)
+
+class Success:
+    def GET(self):
+        content = "OK"
+        return render.success(content) 
+
+class Failure:
+    def GET(self):
+        content = "Failure"
+        return render.failure(content) 
+        
 
 if __name__ == "__main__":
     myapp = web.application(urls, globals())
