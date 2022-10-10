1e2a4dc2a443c5480fbbc03e11f4534f6763a343
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-10-10 18:00:06 +0200

    delete from db in progress

--- webpy/mynotes/app.py
+++ webpy/mynotes/app.py
@@ -3,6 +3,7 @@ import web
 render = web.template.render('templates/')
 urls = (
     '/', 'Home',
+    '/confirm_delete', 'Confirm_delete',
 )
 
 # Connect to db:
@@ -24,6 +25,15 @@ class Home:
         i = web.input()
         n = db.insert('notes', content=i.note)
         raise web.seeother('/')
+
+class Confirm_delete:
+    def GET(self):
+        i = web.input()
+        note_id = i.note_id
+        note = db.select('notes', where="id=$note_id")
+        return render.confirm_delete(note)
+
+
 
 if __name__ == "__main__":
     myapp = web.application(urls, globals())
