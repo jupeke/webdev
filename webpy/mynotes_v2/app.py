da8a7ce1b56fababc4ebacc65a2cb466867c89f6
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-10-14 16:59:39 +0200

    add user works

---
+++ webpy/mynotes_v2/app.py
@@ -0,0 +1,89 @@
+from random import randint
+import web
+render = web.template.render('templates/')
+urls = (
+    '/', 'Home',
+    '/confirm_delete', 'Confirm_delete',
+    '/new','Newnote',
+    '/edit', 'Edit',
+    '/signup', 'Signup'
+)
+
+# Connect to db:
+db = web.database(
+    dbn='mysql',
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    pw='',
+    db='db_mynotes',
+)
+
+class Home:
+    def GET(self):
+        notes = db.select('notes')
+        i = web.input(message="")
+        message = i.message
+        return render.home(notes, message)   # the template name
+    
+    def POST(self):
+        i = web.input(todo="show")
+        todo = i.todo
+        if todo == "delete":
+            note_id = i.note_id
+            raise web.seeother('/confirm_delete?note_id='+note_id)
+        elif todo == "update":
+            note_id = i.note_id
+            raise web.seeother('/edit?note_id='+note_id)
+        elif todo == "show":    # Default
+            raise web.seeother('/') 
+            
+class Newnote:
+    def GET(self):
+        return render.note_new()
+    def POST(self):
+        i = web.input()
+        n=db.insert('notes', content=i.content)
+        raise web.seeother('/')
+
+class Edit:
+    def GET(self):
+        i = web.input()
+        note_id = i.note_id
+        myvar = dict(id=note_id)    # To prevent SQL injection attacks.
+        notes = db.select('notes', vars=myvar, where="id=$id")
+        return render.note_edit(notes[0])
+    def POST(self):
+        i = web.input()
+        note_id = i.note_id
+        cont = i.content
+        myvar = dict(id=note_id)    
+        n=db.update('notes', vars=myvar, where="id=$id", content=cont)
+        raise web.seeother('/')
+
+class Confirm_delete:
+    def GET(self):
+        i = web.input()
+        note_id = i.note_id
+        myvar = dict(id=note_id)
+        notes = db.select('notes', vars=myvar, where="id=$id")
+        return render.confirm_delete(notes[0])
+    def POST(self):
+        i = web.input()
+        note_id = i.note_id
+        myvar = dict(id=note_id)
+        db.delete('notes', vars=myvar, where="id=$id")
+        raise web.seeother('/')
+
+class Signup:
+    def GET(self):
+        return render.signup()
+    def POST(self):
+        i = web.input()
+        id=db.insert('users', name=i.name, username=i.uname, \
+            password=i.pword)
+        raise web.seeother('/?message=user {} created'.format(i.name))
+
+if __name__ == "__main__":
+    myapp = web.application(urls, globals())
+    myapp.run() 
