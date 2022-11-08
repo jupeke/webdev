20a90d621b744d31deb10bce0654539ea048a704
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-11-08 12:49:40 +0100

    image problems

--- webpy/imagetest/app.py
+++ webpy/imagetest/app.py
@@ -15,10 +15,20 @@ class Upload:
     def POST(self):
         x = web.input(myfile={})
         web.debug(x['myfile'].filename) # This is the filename
-        web.debug(x['myfile'].value) # This is the file contents
-        web.debug(x['myfile'].file.read()) # Or use a file(-like) object
+        #web.debug(x['myfile'].value) # This is the file contents
+        #web.debug(x['myfile'].file.read()) # Or use a file(-like) object
+        filedir = 'images' # change this to the directory you want to store the file in.
+        if 'myfile' in x: # to check if the file-object is created
+            filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+            filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+            extension=filename.split('.')[-1] 
+            web.debug(filename)
+            web.debug(extension)
+            fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+            fout.close() # closes the file, upload complete.
         raise web.seeother('/upload')
-
+        # Help?: decode('utf-8')
 
 if __name__ == "__main__":
    app = web.application(urls, globals())
