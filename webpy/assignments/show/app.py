c2924243595e07b8785296333ed71e3e851dc5fa
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-11-11 11:10:12 +0100

    edit show

--- webpy/assignments/show/app.py
+++ webpy/assignments/show/app.py
@@ -4,6 +4,8 @@ import web
 render = web.template.render('templates/')
 urls = (
     '/', 'Home',
+    '/new','Newnote',
+    '/edit','Edit'
 )
 
 # Connect to db:
@@ -29,7 +31,26 @@ class Home:
 class Newnote:
     def GET(self):
         return render.note_new()
+    def POST(self):
+        i = web.input()
+        mycontent = i.content
+        n = db.insert('notes',content=mycontent)
+        raise web.seeother('/')
 
+class Edit:
+    def GET(self):
+        i = web.input()
+        note_id = i.note_id
+        myvar = dict(id=note_id)
+        notes = db.select('notes',vars=myvar,where="id=$id")
+        return render.note_edit(notes[0])
+    def POST(self):
+        i = web.input()
+        note_id = i.note_id
+        cont = i.content
+        myvar = dict(id=note_id)
+        notes = db.update('notes',vars=myvar,where="id=$id",content=cont)
+        raise web.seeother('/')
 
 if __name__ == "__main__":
     myapp = web.application(urls, globals())
