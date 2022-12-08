63b5dcc82f94fe69b33f710e8524f514ef816d3f
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-12-08 12:03:36 +0100

    tictac goes on

---
+++ js/tictac/tictac.js
@@ -0,0 +1,94 @@
+const BOARD_SIZE = 20;
+const EMPTY = "";
+const UNKNOWN = "outo";
+var board = UNKNOWN;
+
+class Board{
+  constructor(){
+    this.table = initiate(BOARD_SIZE,BOARD_SIZE);
+  }
+
+  get_cols(){
+    return this.table[0].length;
+  }
+  get_rows(){
+    return this.table.length;
+  }
+
+  // Set value to the cell at (x,y). Value x refers to col (width),
+  // y to row (height) like in the coordinates system. Note:
+  // x and y begin at 1. Cell (1,1) is the top left one (like in a
+  // spread sheet program).
+  // Return True if success, otherwise False.
+  set(value, x, y){
+    let success = false;
+    if(x > this.get_cols() || y > this.get_rows()){
+        alert("Value too big for the table ("+x+","+y+")!");
+    } else{
+        this.table[y-1][x-1] = value;
+        success = true;
+    }
+    return success;
+  }
+  // Returns the value of cell (x,y).
+  get(x,y){
+    let value = UNKNOWN;
+    if(x > this.get_cols() || y > this.get_rows()){
+        alert("Value too big for the table ("+x+","+y+")!");
+    } else{
+        value = this.table[y-1][x-1];
+        if(value === EMPTY){
+            value = "";
+        }
+    }
+    return value;
+  }
+
+  // Initate array with the right sizes and empty cells:
+  initiate(cols,rows){
+    //alert("(cols,rows)=("+this.cols+","+this.rows+")");
+    let table = new Array(rows);
+    for(let i = 0; i < table.length; i++){
+        table[i] = new Array(cols);
+        for(let k=0; k < cols; k++){
+            table[i][k] = new Cell(k,i); // Note the order.
+        }
+    }
+    return table;
+  }
+  
+  show(){
+    document.querySelector("board").innerHTML = this.toHTML();
+  }
+
+  // Returns an HTML presentation of the board:
+  toHTML(){
+    let html = "<table>";
+
+    // Rows:
+    for(let i = 1; i <= this.get_rows(); i++){
+      html += "<tr>";
+
+      // a Row (there are as many cols as the rich);
+      for(let k = 1; k <= this.get_cols(); k++){
+          html += "<td>"+this.get(k,i)+"</td>";
+      }
+      html += "</tr>";
+    }
+    html += "</table>";
+    return html;
+  }
+}
+
+class Cell{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.value = "";
+  }
+}
+
+function init(){
+  board = new Board();
+  board.show();
+}
