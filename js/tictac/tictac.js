c879546b277371175284a3ceb5550dde6ff1413b
jukka-pekka <11733951+jpkerkkanen@users.noreply.github.com>
2023-01-22 10:52:33 +0100

    from salama

--- js/tictac/tictac.js
+++ js/tictac/tictac.js
@@ -1,15 +1,16 @@
-const BOARD_SIZE = 10;
+const BOARD_WIDTH = 10;
+const BOARD_HEIGHT = 10;
 const EMPTY = "";
 const UNKNOWN = "outo";
 const VAL_X = "value_x";
 const VAL_O = "value_o";
 var board = UNKNOWN;
-var winLength = 5;  //Default.
+var winningLen = 5;  //Default.
 var turn = VAL_X; // Who is ticking
 
 class Board{
   constructor(){
-    this.table = this.initiate(BOARD_SIZE,BOARD_SIZE);
+    this.table = this.initiate(BOARD_WIDTH,BOARD_HEIGHT);
   }
 
   get_cols(){
@@ -46,34 +47,33 @@ class Board{
   }
 
   // Returns the cell object at (x,y), an object of Cell class.
-  // If not found, returns the value UNKNOWN.
+  // If not found (bad value x or y), returns the value false.
   get_cell(x,y){
-    let cell = UNKNOWN;
-    if(x > this.get_cols() || y > this.get_rows()){
-        alert("Value too big for the table ("+x+","+y+")!");
-    } else{
-        cell = this.table[y-1][x-1];
+    let cell = false;
+    if(x >= 1 && x <= this.get_cols() && y >= 1 && y <= this.get_rows()){
+      cell = this.table[y-1][x-1];
     }
     return cell;
   }
-  // Return true if there is a cell next to the one
-  // given as parameter. Otherwise return false. The
-  // direction (dir) must be "east"/ "south" / "northeast" / "southeast". 
-  cellBesideExists(cell, dir){
-    if(dir === "east"){
-      if(cell.x+1 <= BOARD_SIZE){
-        return true;
-      } else{
-        return false;
-      }
-    } else if(dir === "south"){
-    
-    } else if(dir === "northeast"){
-    
-    } else{ // southeast
-
+  // Return the adjacent cell in the given direction or false 
+  // if not possible. Take as parameters the current cell and
+  // a direction (dir) in form "E"/"S"/"NE"/"SE" corresponding
+  // east/south/northeast/southeast. 
+  getNextCell(cell, dir){
+    let result = false;
+    if(dir === "E"){  
+      result = this.get_cell(cell.x+1, cell.y)
+    } 
+    else if(dir === "S"){
+      result = this.get_cell(cell.x, cell.y+1)
+    } 
+    else if(dir === "NE"){
+      result = this.get_cell(cell.x+1, cell.y-1)
+    } 
+    else if(dir === "SE"){ 
+      result = this.get_cell(cell.x+1, cell.y+1)
     }
-  
+    return result;
   }
 
   // Initate array with the right sizes and empty cells:
@@ -102,51 +102,64 @@ class Board{
     cell.showValue();
   }
   // Enough to check 4 cells to all 8 directions from the
-  // tick. Means 4 times 9 cell vector that is to be checked.
-  // Return either VAL_O or VAL_X if winner found or UNKNOWN otherwise.
+  // tick. Means 4 times a 9 cell vector that is to be checked.
+  // Return either true if winner found or false otherwise.
   checkIfFinished(cell){
-    let winner = UNKNOWN;
+    let finished = false;
     let valueToCheck = cell.value;
-    // Check Horizontal:
-    let counter = 0;
-    let currCell = this.checkGetStartPoint("horizontal", cell);
-    for(var i = 1; i <= (2*winLength-1); i++){
-      if(currCell.value === valueToCheck){
-        counter++;
-      } else{
-        counter = 0;
-      }
+    let startCellDirs = ["horizontal", "vertical", "uphill", "downhill"];
+    let nextCellDirs = ["E", "S", "NE", "SE"];
 
+    for(let k = 0; k < startCellDirs.length; k++){
+      let counter = 0;
+      let currCell = this.getStartCellForCheck(startCellDirs[k], cell);
+
+      for(var i = 1; i <= (2*winningLen-1); i++){
+        if(currCell.value === valueToCheck){
+          counter++;
+        } else{
+          counter = 0;
+        }
+        if(counter === winningLen){
+          finished = true;
+          break;
+        } 
+        else{
+          currCell = this.getNextCell(currCell,nextCellDirs[k]);
+
+          // If currCell is au bord du goufre, stop:
+          if(currCell === false){
+            break;
+          } 
+        }
+      }
     }
-    // Check Vertical:
-    // Check Uphill:
-    // Check downhill:
-    return true;
+    return finished;
   }
   // Returns the cell that starts the search vector. The
   // direction and the ticked cell given as parameters.
-  checkGetStartPoint(dir,currCell){
+  getStartCellForCheck(dir,currCell){
     if (dir === "vertical"){
       x = currCell.x;
-      y = max(currCell.y-(winLength-1),1);
+      y = Math.max(currCell.y-(winningLen-1),1);
     } else if (dir === "horizontal"){
-      x = max(currCell.x-(winLength-1),1);
+      x = Math.max(currCell.x-(winningLen-1),1);
       y = currCell.y;
-    } else if (dir === "uphill"){
+    } else if (dir === "uphill"){ // Start point bottom left
       let x = currCell.x;
       let y = currCell.y;
-      for(let i = 1; i < (winLength-1);i++){
-        if(x-1 >= 1 && y+1 <= BOARD_SIZE){
+      for(let i = 1; i < (winningLen-1);i++){
+        if(x-1 >= 1 && y+1 <= BOARD_HEIGHT){
           x--;
           y++;
         } else{
           break;  // The board side reached.
         }
       }
-    } else if (dir === "downhill"){
+    } else if (dir === "downhill"){ // Start point top left
       let x = currCell.x;
       let y = currCell.y;
-      for(let i = 1; i < (winLength-1);i++){
+      for(let i = 1; i < (winningLen-1);i++){
         if(x-1 >= 1 && y-1 >= 1){
           x--;
           y--;
@@ -160,6 +173,7 @@ class Board{
     }
     return this.get_cell(x,y);
   }
+
   // Create an HTML presentation of the board. Return a DOM object.
   createBoard(){
     const table = document.createElement("table");
@@ -172,12 +186,15 @@ class Board{
       for(let k = 1; k <= this.get_cols(); k++){
         let c = row.insertCell(-1); // At the last position
         let cell = this.get_cell(k,i);
+        
         c.setAttribute("id",cell.id);
 
         // So called "arrow function" preserves the scope for 'this'
         c.addEventListener("click", ()=>{ 
           this.tick(cell);
-          this.checkSituation(cell);
+          if(this.checkIfFinished(cell)){
+            alert("Winner found!");
+          }
         });
       } 
     }
