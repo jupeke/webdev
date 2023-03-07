89d9e2dd3e5a786f28838019873a943bc585d2c3
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-03-07 12:55:23 +0100

    Create showindex.php

---
+++ php/showindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <p>This is my great site with PHP.</p>
+    <?php
+        $myvar = "This is done by PHP";
+        echo $myvar;
+    ?>
+</body>
+</html>
