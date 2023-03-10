d818ba1818bfd4c3f3516864634f6af0395ec600
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-03-10 15:19:55 +0100

    edit timer

--- js/util/timer/mytimer.js
+++ js/util/timer/mytimer.js
@@ -6,16 +6,19 @@ const startBtn = document.getElementById("start-button");
 const pauseBtn = document.getElementById("pause-button");
 const progBarElapsed = document.getElementById("progress-bar-elapsed");
 
+let totalTime = 0;
+let startTime = 0;
+
 class Timer{
     constructor(){
-        this.totalTime = 0;
-        this.startTime = 0;
+        //this.totalTime = 0;
+        //this.startTime = 0;
         this.setIntID = -1;
     }
     
     start(){
         // The total time in milliseconds
-        this.totalTime = 
+        totalTime = 
             1000* 
             (
                 parseInt(hours.value) * 60 + 
@@ -24,8 +27,13 @@ class Timer{
             );
 
         // The number of milliseconds since January 1, 1970:
-        this.startTime = Date.now(); 
-        timer.run();
+        //this.startTime = Date.now(); 
+        startTime = Date.now(); 
+        timeLeftDiv.innerHTML = totalTime/1000;
+        //()=>{
+          //  this.run();
+        //}
+        timer.run();    // Note: this.run() does not work, why?
         pauseBtn.disabled = false;
         startBtn.disabled = true;
     }
@@ -34,16 +42,20 @@ class Timer{
         // This thing called arrow function (ES6) works! It
         // preserves the local scope of "this".
         this.setIntID = setTimeout(() => {
-            let elapsedTime = Date.now()-this.startTime;
-            let timeLeft = this.startTime + this.totalTime - elapsedTime;
-            timeLeftDiv.innerHTML = timeLeft;
-            this.run();
+            let endTime = startTime + totalTime;
+            let timeLeft = Math.round((endTime-Date.now())/1000);
+            if(timeLeft < 0){
+                clearTimeout(this.setIntID);
+            } else{
+                timeLeftDiv.innerHTML = timeLeft;
+                this.run();
+            }
         }, 1000);
     }
 
     updateTime(){
-        let elapsedTime = Date.now()-this.startTime;
-        let timeLeft = this.startTime + this.totalTime - elapsedTime;
+        let elapsedTime = Date.now()-startTime;
+        let timeLeft = startTime + totalTime - elapsedTime;
         timeLeft.innerHTML = "hih"+timeLeft;
 
         /*if (elapsedTime > this.totalTime){
