c8876c47c86966e1f4f5c6bc7c8cbd82a3ecce1d
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-03-10 12:12:50 +0100

    timer edits

--- js/util/timer/mytimer.js
+++ js/util/timer/mytimer.js
@@ -1,6 +1,7 @@
 const hoursTextField = document.getElementById("hours");
 const minutesTextField = document.getElementById("minutes");
 const secondsTextField = document.getElementById("seconds");
+const timeLeftDiv = document.getElementById("time-left");
 const startBtn = document.getElementById("start-button");
 const pauseBtn = document.getElementById("pause-button");
 const progBarElapsed = document.getElementById("progress-bar-elapsed");
@@ -8,31 +9,50 @@ const progBarElapsed = document.getElementById("progress-bar-elapsed");
 class Timer{
     constructor(){
         this.totalTime = 0;
+        this.startTime = 0;
         this.setIntID = -1;
     }
     
     start(){
         // The total time in milliseconds
         this.totalTime = 
+            1000* 
             (
                 parseInt(hours.value) * 60 + 
                 parseInt(minutes.value) * 60 + 
                 parseInt(seconds.value)
-            ) * 1000;
+            );
 
         // The number of milliseconds since January 1, 1970:
-        startTime = Date.now(); 
-        endTime = startTime + totalTime;
-
+        this.startTime = Date.now(); 
+        timer.run();
+        pauseBtn.disabled = false;
+        startBtn.disabled = true;
+    }
+    
+    run(){
         // This thing called arrow function (ES6) works! It
         // preserves the local scope of "this".
         this.setIntID = setTimeout(() => {
-            this.updateTimea();
+            let elapsedTime = Date.now()-this.startTime;
+            let timeLeft = this.startTime + this.totalTime - elapsedTime;
+            timeLeftDiv.innerHTML = timeLeft;
+            this.run();
         }, 1000);
     }
-    updateTime(){
 
+    updateTime(){
+        let elapsedTime = Date.now()-this.startTime;
+        let timeLeft = this.startTime + this.totalTime - elapsedTime;
+        timeLeft.innerHTML = "hih"+timeLeft;
+
+        /*if (elapsedTime > this.totalTime){
+            clearInterval(this.setIntID);
+            pauseBtn.disabled = true;
+            startBtn.disabled = false;
+        }*/
     }
+
     pause(){
         
     }
