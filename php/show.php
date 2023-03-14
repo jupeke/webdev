428eee4af3854c6e689b2f9d5ccec8fe0106757c
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-03-14 12:52:24 +0100

    Update show.php

--- php/show.php
+++ php/show.php
@@ -2,27 +2,21 @@
   <head>
     <title>PHP Home</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
   </head>
 
   <body>
     <h1>My first form (GET)</h1>
-    <p>Here the client (browser) sends an HTTP request 
-        to the server by using the GET method.</p>
     <?php
-      if (isset($_GET["fname"]) && !empty($_GET["myname"])){
-        $message = "Hello ".$_GET["myname"]."!";
-      } else{
-        $message = "Nothing yet sent";
+      function message($name){
+        return "Hello ".$name."!";
       }
-      echo $message;
+      $name = $_GET["myname"];
+      echo message($name);
     ?>
     <p>Write you name in the text field and press the button.</p>
     <form method="get">
-      <label for="fname">Your firstname:</label><br>
-      <input type="text" id="fname" name="fname"><br>
-      <label for="lastname">Your lastname:</label><br>
-      <input type="text" id="lastname" name="lastname"><br>
+      <label for="name">Your firstname:</label><br>
+      <input type="text" id="name" name="myname"><br>
       <input type="submit" value="Submit">
     </form>
   </body>
